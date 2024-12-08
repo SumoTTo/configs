5357d6844f0b0af244a41791cc4c3f908091a448
Kirill Sigalin <kirill@eastsummit.com>
2024-12-08 21:00:49 +0300

    Fixed ignoring in clean plugin.

--- helpers/clean-webpack-plugin.js
+++ helpers/clean-webpack-plugin.js
@@ -1,5 +1,6 @@
 const glob = require( 'glob' );
 const rimraf = require( 'rimraf' );
+const fs = require( 'fs' );
 
 let isRunning = false;
 
@@ -28,8 +29,50 @@ class CleanWebpackPlugin {
 	}
 
 	del() {
-		glob.sync( this.patterns ).forEach( ( file ) => {
+		const inclusionPatterns = this.patterns.filter(
+			( pattern ) => ! pattern.startsWith( '!' )
+		);
+		const exclusionPatterns = this.patterns
+			.filter( ( pattern ) => pattern.startsWith( '!' ) )
+			.map( ( pattern ) => pattern.slice( 1 ) );
+
+		let directories = [];
+		let filesToDelete = [];
+		inclusionPatterns.forEach( ( pattern ) => {
+			const results = glob.sync( pattern, { withFileTypes: true } );
+			results.forEach( ( result ) => {
+				if ( result.isDirectory() ) {
+					directories.push( result.fullpath() );
+				} else {
+					filesToDelete.push( result.fullpath() );
+				}
+			} );
+		} );
+
+		let excludedFiles = [];
+		exclusionPatterns.forEach( ( pattern ) => {
+			const files = glob.sync( pattern, { nodir: true } );
+			excludedFiles = excludedFiles.concat( files );
+		} );
+
+		directories = excludedFiles.filter(
+			( e, index ) => directories.indexOf( e ) === index
+		);
+		excludedFiles = excludedFiles.filter(
+			( e, index ) => excludedFiles.indexOf( e ) === index
+		);
+		filesToDelete = filesToDelete
+			.filter( ( e, index ) => filesToDelete.indexOf( e ) === index )
+			.filter( ( file ) => ! excludedFiles.includes( file ) );
+
+		filesToDelete.forEach( ( file ) => {
 			rimraf.sync( file, { glob: false } );
+		} );
+
+		directories.forEach( ( directory ) => {
+			if ( fs.readdirSync( directory ).length === 0 ) {
+				fs.rmdirSync( directory );
+			}
 		} );
 	}
 }
