32386a80a8bb63b2b782836318bef9ce2ccbe690
Kirill Sigalin <kirill@eastsummit.com>
2024-12-14 17:49:53 +0300

    Updated Woo config.

--- wp/webpack.the-woocommerce.config.js
+++ wp/webpack.the-woocommerce.config.js
@@ -1,12 +1,47 @@
 const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+const RemoveEmptyScriptsPlugin = require( 'webpack-remove-empty-scripts' );
+const RtlCssPlugin = require( 'rtlcss-webpack-plugin' );
 const CleanWebpackPlugin = require( '../helpers/clean-webpack-plugin' );
 const SyncWebpackPlugin = require( '../helpers/sync-webpack-plugin' );
 const { resolve } = require( 'node:path' );
-const { Config, modulesConfigWP } = require( '../helpers/webpack' );
+const { Config, defaultConfigWP, modulesConfigWP } = require( '../helpers/webpack' );
+const findFreePort = require( 'find-free-port-sync' );
 const rootPath = process.cwd().replace( /\\/g, '/' );
 const outputPath = process.env.WP_CONTENT_DIR
 	? resolve( process.env.WP_CONTENT_DIR, 'plugins/the-woocommerce' )
 	: rootPath;
+const port =
+	process.env.WOOCOMMERCE_DEV_SERVER_PORT ||
+	findFreePort( { start: 10000, end: 10999 } );
+
+const defaultConfig = new Config( defaultConfigWP, 'default', port )
+	.resetEntries()
+	.addEntries(
+		'src/styles/{*.{pc,sc,sa,c}ss,{blocks,variations,patterns}/**/*.{pc,sc,sa,c}ss}'
+	)
+	.addEntries( 'src/scripts/*.{j,t}s' )
+	.changeRule( '/\\.(sc|sa)ss$/', ( rule ) => {
+		const last = rule.use.length - 1;
+
+		rule.use[ last ].options.sassOptions = {
+			includePaths: [ resolve( rootPath, 'src/styles/partials' ) ],
+		};
+	} )
+	.removePlugin( RtlCssPlugin )
+	.addPlugin(
+		new CleanWebpackPlugin( {
+			patterns: [
+				resolve( outputPath, 'build/**/*' ),
+				'!' + resolve( outputPath, 'build/scripts/modules/**/*' ),
+			],
+		} )
+	)
+	.addPlugin(
+		// For styles remove JS and styles .asset.php
+		new RemoveEmptyScriptsPlugin( {
+			enabled: ! Config.hasDevServer( defaultConfigWP ),
+		} )
+	);
 
 const modulesConfig = new Config( modulesConfigWP, 'modules' )
 	.resetEntries()
@@ -30,7 +65,9 @@ const modulesConfig = new Config( modulesConfigWP, 'modules' )
 	);
 
 if ( process.env.WP_CONTENT_DIR ) {
-	modulesConfigWP.output.path = resolve( outputPath, 'build' );
+	const buildPath = resolve( outputPath, 'build' );
+	defaultConfigWP.output.path = buildPath;
+	modulesConfigWP.output.path = buildPath;
 
 	const syncDirectory = new SyncWebpackPlugin( {
 		sourceDir: rootPath,
@@ -38,7 +75,8 @@ if ( process.env.WP_CONTENT_DIR ) {
 		exclude: [ /[\/\\]build[\/\\]/ ],
 	} );
 
+	defaultConfig.addPlugin( syncDirectory );
 	modulesConfig.addPlugin( syncDirectory );
 }
 
-module.exports = [ modulesConfig.get() ];
+module.exports = [ defaultConfig.get(), modulesConfig.get() ];
