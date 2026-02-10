738ba15934fc70b648f6dd5fec5e855feb0b7ef7
Kirill Sigalin <kirill@eastsummit.com>
2026-02-10 13:36:37 +0300

    Updated postcss config for exclude node modules from wrap selector.

--- wp/postcss.theme.config.js
+++ wp/postcss.theme.config.js
@@ -1,14 +1,26 @@
 const { resolve } = require( 'node:path' );
 
+const shouldApplyWrapSelector = ( filePath ) => {
+	if ( ! filePath ) {
+		return true;
+	}
+
+	return ! /node_modules/.test( filePath );
+};
+
 module.exports = ( api ) => {
 	const isProduction = api.mode === 'production';
+	const currentFile = api.file;
+	const applyWrapSelector = shouldApplyWrapSelector( currentFile );
 
 	return {
 		ident: 'postcss',
 		sourceMap: ! isProduction,
 		plugins: [
 			[ 'autoprefixer', { grid: true } ],
-			[ require( resolve( __dirname, '../postcss/wrap-selector.js' ) ) ],
+			applyWrapSelector && [
+				require( resolve( __dirname, '../postcss/wrap-selector.js' ) ),
+			],
 			isProduction && [
 				'cssnano',
 				{
